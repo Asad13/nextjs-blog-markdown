ec558b39842d35e4305550863d94f9b301be0bce
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-10-11 17:38:35 +0600

    404 Page, 500 Page, cssnano Two pages added to handle errors. 404.js to handle page not not found. 500.js to handle server error. cssnano package is installed added in the postcss.config.js to minify the css is production.

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+    return (
+        <div className="flex justify-center items-center h-[72vh]">
+            <h1 className='text-xl md:text-2xl lg:text-5xl text-slate-800 dark:text-white font-black text-center'>404 - Page Not Found</h1>
+        </div>
+    )
+}
---
+++ pages/500.js
@@ -0,0 +1,7 @@
+export default function Custom500() {
+    return (
+        <div className="flex justify-center items-center h-[72vh]">
+            <h1 className='text-xl md:text-2xl lg:text-5xl text-slate-800 dark:text-white font-black text-center'>500 - Server-side error occurred</h1>
+        </div>
+    );
+  }
